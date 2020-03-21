2d76961dcf15788b47ec25cd0219c5bc61826a4d
Biyas Basak <biyas@phypal.com>
2020-03-21 04:54:48 -0400

    minimax algorithm implemented

--- main.py
+++ main.py
@@ -6,20 +6,15 @@ class Game:
     def __init__(self, n, player):
         self.size = n
         self.player = player
+        self.opponent = "O" if player == 'X' else 'X'
         self.current_player = 'X'  # initial player is X
-        self.board = [[0 for x in range(n)] for x in range(n)]
+        self.board = [[None for x in range(n)] for x in range(n)]
         self.move_count = n ** n
         self.scores = {
             "X": 10,
             "O": -10,
             "tie": 0
         }
-
-    def is_move_available(self):
-        if (self.moveCount == 0):
-            return False
-        else:
-            return True
 
     def check_winner(self):
         # check rows for winner
@@ -117,34 +112,88 @@ class Game:
         if (self.move_count == 0):
             return 'tie'
         return None
-    
+
+    def flip_player(self):
+        if (self.current_player == 'X'):
+            self.current_player = 'O'
+        else:
+            self.current_player = 'X'
+
     def best_possible_move(self):
         print("minimax algorithm")
+        best_score = -sys.maxsize
+        best_move = None
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if (self.board[i][j] == None):
+                    self.board[i][j] = self.current_player
+                    score = self.minimax(0, True)
+                    if (score > best_score):
+                        best_move = (i, j)
+                    self.board[i][j] = None
+        return best_move
 
     def minimax(self, depth, isMax):
+        winner = self.check_winner()
+        score = 0
+        if (winner):
+            score = self.scores[winner]
+            return score
         if (isMax):
-            score = self.best_possible_move()
+            maxScore = -sys.maxsize
+            for i in range(len(self.board)):
+                for j in range(len(self.board[i])):
+                    if (self.board[i][j] == None):
+                        self.board[i][j] = self.player
+                        score = self.minimax(depth+1, False)
+                        maxScore = max(score, maxScore)
+                        self.board[i][j] = None
+            return maxScore
         else:
-            score = self.best_possible_move()
+            minScore = sys.maxsize
+            for i in range(len(self.board)):
+                for j in range(len(self.board[i])):
+                    if (self.board[i][j] == None):
+                        self.board[i][j] = self.opponent
+                        score = self.minimax(depth+1, True)
+                        minScore = min(score, minScore)
+                        self.board[i][j] = None
+            return minScore
 
-    def make_move(self):
-        print("----")
-
+    def make_move(self, move):
+        print(move)
+        self.board[int(move[0])][int(move[1])] = self.current_player
+        self.move_count = self.move_count - 1
     # recursively calls itself after every move
+
     def play(self):
+        winner = self.check_winner()
+        if (winner):
+            print(f"And the winner is {winner}")
+            print(self.board)
+            return
         if (self.current_player == self.player):
             best_move = self.best_possible_move()
             self.make_move(best_move)
-            print(best_move)
+            # print(best_move)
         else:
             print("Enter opponent's move")
             move = input()
-            make_move(move)
-        play()
+            move = tuple(move.split(","))
+            self.make_move(move)
+            # print(move)
+        self.flip_player()
+        self.play()
+
+    def display_board(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                print(self.board[i][j], end=' ')
+            print()
 
 
 print("Input the size of the game")
-board_size = input()
+board_size = int(input())
 
 print("Am I X or O ?")
 
@@ -154,7 +203,3 @@ board = Game(board_size, player)
 
 board.play()
 
-
-# board = Game()
-# winner = board.check_winner()
-# print(winner)
