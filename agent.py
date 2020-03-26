ca136dd5eaef434d615769e069008b37ced64ee9
Dishit <dishitkotecha@gmail.com>
2020-03-26 19:40:41 -0400

    Added create game api call

--- agent.py
+++ agent.py
@@ -8,10 +8,25 @@ class agent:
         self.state = ''
         self.baseURL = "http://www.notexponential.com/aip2pgaming/api/index.php"
 
+    def create_a_game(self, teamId1, teamId2):
+        payload={
+            "teamId1": teamId1,
+            "teamId2": teamId2,
+            "type": "game",
+            "gameType": "TTT"
+        }
+        headers ={
+            "x-api-key": "21be1b176c5d8c7bc09c",
+            "userid": "843",
+            "User-Agent": "Mozilla / 5.0(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+        }
+        response = requests.post(self.baseURL, headers=headers, data=payload)
+        print(response.text)
+
     def get_board_string(self, gameId):
         headers = {
-            'x-api-key': '21be1b176c5d8c7bc09c',
-            'userid': '843',
+            "x-api-key": "21be1b176c5d8c7bc09c",
+            "userid": "843",
             "User-Agent": "Mozilla / 5.0(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
         }
         payload = {
@@ -63,10 +78,10 @@ class agent:
         response = requests.post(self.baseURL, headers=headers, data=payload)
         print(response.text)
 
-def test():
-    closeAI = agent()
-    closeAI.get_board_string(87)
-    closeAI.get_board_map(87)
-    closeAI.get_moves(87, 20)
-    closeAI.make_move({6, 8},95)
-test()
+# def test():
+#     closeAI = agent()
+#     closeAI.get_board_string(87)
+#     closeAI.get_board_map(87)
+#     closeAI.get_moves(87, 20)
+#     closeAI.make_move({6, 8},95)
+# test()
