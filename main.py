c718aeac222deafdcddc4b7576dc45f8693a2ebe
Biyas Basak <biyas@phypal.com>
2020-03-31 02:50:56 -0400

    unused variables removed

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 import random
 import sys
 
-
 class Game:
     def __init__(self, n, player):
         self.size = n
         self.target = int(n / 2)
-        self.player = 'X' if player == 'X' else 'O'
-        self.opponent = 'O' if player == 'X' else 'X'
+        self.player = player
+        self.opponent = "O" if player == 'X' else 'X'
         self.current_player = 'X'  # initial player is X
         self.board = [[None for x in range(n)] for x in range(n)]
-        self.move_count = n ** n
         self.scores = {
             "X": 10,
             "O": -10,
-            "Draw": 0
+            "tie": 0
         }
         self.alpha = -sys.maxsize
         self.beta = sys.maxsize
 
-    def score_selection(self):
+    def scoreSelection(self):
         if self.player == 'O':
             self.scores = {
                 "X": -10,
                 "O": 10,
-                "Draw": 0
+                "tie": 0
             }
 
     def check_winner(self):
@@ -115,107 +113,50 @@ class Game:
         if winner:
             return winner
 
-        flag = 'Draw'
+        flag = 'tie'
         for i in range(self.size):
             for j in range(self.size):
                 if self.board[i][j] == None:
                     flag = None
                     break
-        return flag
-
-    def flip_player(self):
-        if self.current_player == 'X':
-            self.current_player = 'O'
-        else:
-            self.current_player = 'X'
-
-    def best_possible_move(self):
-        print("Computing best move for {}...".format(self.current_player))
-        best_score = -sys.maxsize
-        best_move = None
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                if self.board[i][j] is None:
-                    self.board[i][j] = self.current_player
-                    score = self.minimax(0, False)
-                    if score > best_score:
-                        best_move = (i, j)
-                        best_score = score
-                    self.board[i][j] = None
-        return best_move
-
-    def minimax(self, depth, isMax):
-        winner = self.check_winner()
-        score = 0
-        if winner:
-            score = self.scores[winner]
-            return score
-        if isMax:
-            maxScore = -sys.maxsize
-            if score < self.alpha:
-                return self.alpha
-            for i in range(len(self.board)):
-                for j in range(len(self.board[i])):
-                    if self.board[i][j] is None:
-                        self.board[i][j] = self.player
-                        score = self.minimax(depth+1, False)
-                        maxScore = max(score, maxScore)
-                        self.board[i][j] = None
-            self.alpha = max(maxScore, self.alpha)
-            return maxScore
-        else:
-            minScore = sys.maxsize
-            if self.beta < score:
-                return self.beta
-            for i in range(len(self.board)):
-                for j in range(len(self.board[i])):
-                    if self.board[i][j] is None:
-                        self.board[i][j] = self.opponent
-                        score = self.minimax(depth+1, True)
-                        minScore = min(score, minScore)
-                        self.board[i][j] = None
-            self.beta = max(minScore, self.beta)
-            return minScore
+        return flag   # check rows for winner
 
     def make_move(self, move):
         print(move)
-        self.board[int(move[0])][int(move[1])] = self.current_player
-        self.move_count = self.move_count - 1
+        self.board[move[0]][move[1]] = self.current_player
     # recursively calls itself after every move
 
     def play(self):
-        self.score_selection()
+        self.scoreSelection()
         if self.current_player == self.player:
             best_move = self.best_possible_move()
             self.make_move(best_move)
         else:
             print("Enter opponent's move")
             move = input()
-            move = tuple(move.split(","))
+            move = tuple(map(int, move.split(",")))
             self.make_move(move)
+            # print(move)
         winner = self.check_winner()
-        if winner and winner != 'Draw':
-            print("And the winner is {}".format(winner))
-            self.display_board()
+        if winner and winner != 'tie':
+            print(f"And the winner is {winner}")
+            print(self.board)
             return
-        elif winner == 'Draw':
-            print('Draw')
-            self.display_board()
+        elif winner == 'tie':
+            print('It is a tie')
+            print(self.board)
             return
         self.flip_player()
         self.play()
 
     def display_board(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.board[i][j]:
-                    print(self.board[i][j], end=' ')
-                else:
-                    print('-', end=' ')
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                print(self.board[i][j], end=' ')
             print()
 
 
-print("Input the size of the game:")
+print("Input the size of the game")
 board_size = int(input())
 
 print("Am I X or O ?")
